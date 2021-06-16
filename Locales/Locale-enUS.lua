bcb237ba7fe6be154caded50eccd95ea5496886f
David Hedbor <neotron@gmail.com>
2021-06-16 12:06:58 +0200

    Fixed localization string warning

--- Locales/Locale-enUS.lua
+++ Locales/Locale-enUS.lua
@@ -1,8 +1,69 @@
-local debug = false
+local silent = true
 --@debug@
-debug = true
+silent = false
 --@end-debug@
 
-local L = LibStub("AceLocale-3.0"):NewLocale("BulkMailInbox", "enUS", true, debug)
+local L = LibStub("AceLocale-3.0"):NewLocale("BulkMailInbox", "enUS", true, silent)
 
+
+L["Alt - Delete Containing Mail"] = true
+L["Alt-click Delete"] = true
+L["Always Take Stackable Items"] = true
+L["Bulk Mail Inbox"] = true
+L["Bulk Mail Inbox Options"] = true
+L["Cancel"] = true
+L["Cancel taking items from the inbox."] = true
+L["Cash"] = true
+L["Clear Selected"] = true
+L["Clear the list of selected items."] = true
+L["Close"] = true
+L["Close the window and stop taking items from the inbox."] = true
+L["Continue taking partial stacks of stackable items even if the mailbox is full."] = true
+L["Ctrl - Return Containing Mail"] = true
+L["Ctrl-click Return"] = true
+L["Disable Tooltips"] = true
+L["Disable the help tooltips for the toolbar buttons."] = true
+L["Enable 'Take All' button in inbox."] = true
+L["Enable Alt-Click on inbox items to delete the mail in which they are contained."] = true
+L["Enable Ctrl-click on inbox items to return the mail in which they are contained."] = true
+L["Enable Shift-click on inbox items to take them."] = true
+L["Filter the inbox display to items matching the term entered here.\nTake All and Take Cash actions are limited to items matching the inbox filter."] = true
+L["Font"] = true
+L["Font size"] = true
+L["Font used in the inbox list"] = true
+L["GUI Scale"] = true
+L["Go to the next page of items."] = true
+L["Go to the previous page of items."] = true
+L["Hint:"] = true
+L["Inbox"] = true
+L["Inbox Items (%d mails, %s)"] = true
+L["Item %d-%d"] = true
+L["Item %d-%d of %d"] = true
+L["Items (Inbox click actions apply)"] = true
+L["Left click to open the config panel."] = true
+L["Mail #"] = true
+L["Next Page"] = true
+L["No"] = true
+L["No items"] = true
+L["Previous Page"] = true
+L["Qty."] = true
+L["Returnable"] = true
+L["Right click to open the config menu."] = true
+L["Search"] = true
+L["Sender"] = true
+L["Set the window scale of the Inbox GUI."] = true
+L["Shift - Take Item"] = true
+L["Shift-click Take"] = true
+L["Show Inbox GUI"] = true
+L["Show the Inbox Items GUI"] = true
+L["TTL"] = true
+L["Take All"] = true
+L["Take Cash"] = true
+L["Take Selected"] = true
+L["Take all items from the mailbox. If the search filter is used,\nonly items matching the search term will be taken."] = true
+L["Take all money from the mailbox. If the search filter is used,\nmoney will only be taken from mails which the search term."] = true
+L["Take all selected items from the mailbox."] = true
+L["Yes"] = true
+L["Page Size"] = true
+L["Maximum number of items to display per page."] = true
 --@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
